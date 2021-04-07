6f8de617df8b66e751962ba4e8f9adb80766c2b2
himanshu76200 <himanshu76200@gmail.com>
2021-04-07 23:34:36 +0530

    completed first build

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import "./Nav.css";
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+                className="nav__logo"
+                src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+                alt=""
+            />
+            <img
+                className="nav__avatar"
+                src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Nav;
